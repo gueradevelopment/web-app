d5f2039cb3061752263189767352e7b72af81093
Miguel Montoya <miguel.mzbi@gmail.com>
2019-02-06 19:59:11 -0600

    Mock Menu

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="menu-container">
+        <p> {{ msgProp }} </p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Sidebar extends Vue {
+    @Prop() private msgProp!: string;
+
+    msg = "MIGUEL"
+
+    // constructor() {
+    //     super();
+    //     this.msgProp = "MIKAELO";
+    // }
+
+    mounted() {
+        alert("Hello " + this.msg);
+    }
+}
+</script>
+
+<style scoped lang="scss">
+#menu-container {
+    position: absolute;
+    background-color: red;
+    width: 15%;
+    min-width: 200px;
+    height: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Sidebar msgProp="MIKAELO"/>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
@@ -8,10 +9,12 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Sidebar from '@/components/Sidebar.vue';
 
 @Component({
   components: {
     HelloWorld,
+    Sidebar,
   },
 })
 export default class Home extends Vue {}
