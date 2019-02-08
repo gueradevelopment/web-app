23c6a388079f8d7eb29a5225a660e736999ed046
Miguel Montoya <miguel.mzbi@gmail.com>
2019-02-07 20:22:49 -0600

    Basic Left Menu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
+    <Sidebar/>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <div id="viewer">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -15,6 +18,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+#viewer {
+  position: absolute;
+  left: 300px;
 }
 #nav {
   padding: 30px;
@@ -27,3 +34,15 @@
   }
 }
 </style>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Sidebar from '@/components/Sidebar.vue';
+
+@Component({
+  components: {
+    Sidebar,
+  },
+})
+export default class App extends Vue {}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="home">
-    <Sidebar msgProp="MIKAELO"/>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img src="../assets/logo_name.svg" width="50%">
+    <h1>Welcome to Guera</h1>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import Sidebar from '@/components/Sidebar.vue';
 
-@Component({
-  components: {
-    HelloWorld,
-    Sidebar,
-  },
-})
 export default class Home extends Vue {}
 </script>
