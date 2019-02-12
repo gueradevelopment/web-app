dc23a0c29716756f18f4c42d45d70fbf472a42e0
geraJuarez <geracjuarez@gmail.com>
2019-02-11 22:26:28 -0600

    checklistdetail component

--- src/components/test_TaskDetail.vue
+++ src/components/test_TaskDetail.vue
@@ -1,5 +1,13 @@
 <template>
-  <TaskDetail :task="currentTask" />
+  <div>
+    <TaskDetail :task="currentTask" />
+    <br>
+    <br>
+    <h2>Testing the checklist compoenent detail</h2>
+    <div>
+      <CheckListDetail title="My checklist" :tasks="tasks" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,6 +15,7 @@ import Vue from 'vue'
 import {Route} from 'vue-router'
 import Component from 'vue-class-component';
 import TaskDetail from './TaskDetail.vue';
+import CheckListDetail from './CheckListDetail.vue'
 
 interface WithRoute {
 	$route: Route
@@ -20,7 +29,8 @@ interface Task {
 
 @Component({
   components: {
-    TaskDetail
+    TaskDetail,
+    CheckListDetail
   }
 })
 export default class test_TaskDetail extends Vue implements WithRoute {
@@ -33,6 +43,7 @@ export default class test_TaskDetail extends Vue implements WithRoute {
     this.id = 0;
     this.tasks[1] = { title: "Task 1", description: "this is the Task 1", completed: false}
     this.tasks[2] = { title: "Task 2", description: "this is the Task 2", completed: true}
+    this.tasks[3] = { title: "Task 3", description: "this is the Task 3 and has a desc", completed: true}
     this.currentTask = { title: "title", description: "default blah blah", completed: false}
   }
 
