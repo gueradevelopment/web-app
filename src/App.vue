9026416525565cce4ea1f23c63c7ae627c811fb9
geraJuarez <geracjuarez@gmail.com>
2019-02-11 18:04:38 -0600

    testing typescript retrieving data from data route params

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
     <div id="content" @click="deactivate" :class="{ sidebarActive }">
       <div id="nav">
         <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
+        <router-link to="/about">About</router-link> |
+        <router-link to="/task/1">task</router-link>
       </div>
       <div id="viewer">
         <router-view/>
---
+++ src/components/test_TaskDetail.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="task detail">
+    <h1>This is an task page {{id}}</h1>
+    <h2>{{currentTask}}</h2>
+    
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {Route} from 'vue-router'
+import Component from 'vue-class-component';
+
+interface WithRoute {
+	$route: Route
+}
+
+interface Task {
+  description: string;
+  completed: boolean;
+}
+
+@Component
+export default class TaskDetail extends Vue implements WithRoute {
+  id: number;
+  tasks: { [id: number] : Task; } = {};
+  currentTask: Task;
+
+  constructor() {
+    super();
+    this.id = 0;
+    this.tasks[1] = { description: "Task 1", completed: false}
+    this.tasks[2] = { description: "Task 2", completed: true}
+
+    // 
+    this.currentTask = { description: "default blah blah", completed: false}
+  }
+
+  created () {
+    this.id = Number(this.$route.params.id)
+    this.currentTask = this.tasks[this.id]; // simulate data retrieving
+  }
+
+}
+</script>
